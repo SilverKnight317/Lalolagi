6996d6d770e210982e3f6ea691fd9aa7112c7235
SilverKnight317 <ajclarkson3@gmail.com>
2021-11-25 22:56:46 -0500

    Cleaned OffScreen file for later use

--- Scripting/HandleOffScreenActions.cs
+++ Scripting/HandleOffScreenActions.cs
@@ -14,21 +14,7 @@ namespace Lalolagi
         }
         public override void Execute(Dictionary<string, List<Actor>> cast)
         {
-            // throw new System.NotImplementedException();
-            List<Actor> balls = cast["balls"];
-            foreach(Actor ball in balls)
-            {
-                if (ball.GetRightEdge() >= Constants.MAX_X || ball.GetLeftEdge() <= 0)
-                {
-                    ball.ChangeVelocityX();
-                    // _audioService.PlaySound(Constants.SOUND_BOUNCE);
-                }
-                if (ball.GetBottomEdge() >= Constants.MAX_Y || ball.GetTopEdge() <= 0)
-                {
-                    ball.ChangeVelocityY();
-                    // _audioService.PlaySound(Constants.SOUND_BOUNCE);
-                }
-            }
+            throw new System.NotImplementedException();
         }
     }
 }
